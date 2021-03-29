23b31c1899a0f7a684768fa5502b478830773a4b
Héctor Reyes López <hector.reyes.lopez.17@gmail.com>
2021-03-29 14:17:51 +0200

    Fix statics path

--- src/api/core/middlewares/static-content.ts
+++ src/api/core/middlewares/static-content.ts
@@ -3,7 +3,7 @@ const path = require('path');
 export const staticContent = (server) => {
 	if (process.env.ENVIRONMENT === 'production') {
 		server.register(require('fastify-static'), {
-			root: path.join(__dirname, 'src/app')
+			root: path.resolve(__dirname, '../../../app')
 		})
 	}
 }
